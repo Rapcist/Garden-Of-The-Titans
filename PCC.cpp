7ad6bf1860b09a7f53b3443fc18aa92378f4fe17
Rapcist <138100279+Rapcist@users.noreply.github.com>
2023-07-12 13:24:55 +0300

    Update PCC.cpp

--- PCC.cpp
+++ PCC.cpp
@@ -3,9 +3,7 @@ class polynom{
   size_t size;
   int *arr;
 public:
-  polynom(size_t size_) : size(size_){
-    arr = new int[size_];   
-  }
+  polynom(size_t size_) : size(size_), arr(new int[size_]) {}
   size_t length () const {
     return size;
   }
