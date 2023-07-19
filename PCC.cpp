6a8e535095dfb54a55fa98d77d321207816bda19
Rapcist <138100279+Rapcist@users.noreply.github.com>
2023-07-19 10:54:43 +0300

    Update PCC.cpp

--- PCC.cpp
+++ PCC.cpp
@@ -1,10 +1,36 @@
-#define SIZE 30
+
+const int SIZE = 30;
 class polynom{
-  size_t size;
+  unsigned size;
   int *arr;
 public:
-  polynom(size_t size_) : size(size_), arr(new int[size_]) {}
-  size_t length () const {
+  polynom(unsigned size_) : size(size_), arr(new int[size_]) {}
+  unsigned length () const {
     return size;
   }
 };
+
+//---------------------
+
+template <typename T> class loc{
+    public:
+    T show();
+};
+
+template <class T>
+T loc<T>::show() {return (T)0;}
+
+
+//function member specialization 
+
+template <>
+int loc<int>::show() {return 0;}
+
+// class specialization 
+template <> class loc<double>{
+    public:
+    double show();
+};
+double loc<double>::show(){return 0;}
+
+
