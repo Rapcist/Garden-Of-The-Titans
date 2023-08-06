0000a732bda1168668c0e05022af8856354f0d85
Rapcist <138100279+Rapcist@users.noreply.github.com>
2023-08-06 22:01:20 +0300

    Update STR-STL-SHALLENGE.cpp

--- STR-STL-SHALLENGE.cpp
+++ STR-STL-SHALLENGE.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+class StrType{
+  vector<char> s;
+public:
+
+  StrType();
+  StrType(const StrType &ob);
+  StrType(const char *cs);
+
+  friend istream & operator>>(istream &stream, const StrType & in);
+  friend ostream & operator<<(ostream &stream, const StrType & out);
+
+  StrType operator=(const StrType &ob);
+  StrType operator=(const char * cs);
+};
+
+StrType::StrType(){
+  //????
+}
+StrType::StrType(const StrType &ob){
+  
+}
