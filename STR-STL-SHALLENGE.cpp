9b9e6cab0b737976877a246385b8be0ab0f966dc
Rapcist <138100279+Rapcist@users.noreply.github.com>
2023-08-07 11:27:53 +0300

    Update STR-STL-SHALLENGE.cpp

--- STR-STL-SHALLENGE.cpp
+++ STR-STL-SHALLENGE.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+//#include <iostream>
 #include <algorithm>
-
-using namespace std;
+#include <vector>
+#include <iterator>
+//using namespace std;
 
 class StrType{
   vector<char> s;
@@ -11,8 +12,8 @@ public:
   StrType(const StrType &ob);
   StrType(const char *cs);
 
-  friend istream & operator>>(istream &stream, const StrType & in);
-  friend ostream & operator<<(ostream &stream, const StrType & out);
+  friend istream & operator>>(istream &stream, const StrType & ob);
+  friend ostream & operator<<(ostream &stream, const StrType & ob);
 
   StrType operator=(const StrType &ob);
   StrType operator=(const char * cs);
@@ -31,5 +32,33 @@ StrType::StrType(const StrType &ob){
 }
 
 StrType::StrType(const char *cs){
-  
+  for(int i=0;cs[i];++i){
+    s.push_back(cs[i]);
+  }
+}
+istream & operator>>(istream &stream, const StrType & ob){
+  char c[255];
+  stream>>c;
+  for(int i=0;c[i];++i){
+    s.push_back(c[i]);
+  }
+  return stream;
+}
+
+ostream & operator<<(ostream &stream, const StrType & ob){
+  vector<char>::iterator p=s.begin();
+  while(p!=s.end()){
+    stream>>*p++;
+  }
+  return stream;
+}
+
+StrType::StrType operator=(const StrType &ob){
+  s.assign(ob.s.begin(),ob.s.end());
+}
+
+StrType::StrType operator=(const char * cs){
+  int i{};
+  for(;cs[i];++i);
+  s.assign(cs,cs+i);
 }
