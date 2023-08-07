11b3cd5b1683c35eb5957c1f72438c2f627cac57
Rapcist <138100279+Rapcist@users.noreply.github.com>
2023-08-07 12:43:47 +0300

    Update STR-STL-SHALLENGE.cpp

--- STR-STL-SHALLENGE.cpp
+++ STR-STL-SHALLENGE.cpp
@@ -1,8 +1,8 @@
-//#include <iostream>
 #include <algorithm>
 #include <vector>
 #include <iterator>
-//using namespace std;
+
+namespace MyStr{
 
 class StrType{
   vector<char> s;
@@ -22,7 +22,57 @@ public:
   StrType operator+(const char* cs);
   friend StrType operator+(const char* cs,const StrType &ob);
 
-  
+  StrType operator-(const StrType &ob);
+  StrType operator-(const char* cs);
+
+  //relational with StrType
+  bool operator ==(const StrType & ob) {
+    return equal(s.begin(),s.end(),ob.s.begin(),ob.s.end());
+  }
+  bool operator <(const StrType & ob){
+    return lexicographical_compare(s.begin(),s.end(),ob.s.begin(),ob.s.end());
+  }
+
+  bool operator !=(const StrType & ob) {return !(*this==ob);}
+  bool operator >=(const StrType & ob) {return !(*this<ob);}
+  bool operator >(const StrType & ob) {return !(*this<ob && *this!=ob);}
+  bool operator <=(const StrType & ob) {return !(*this>ob);}
+
+  //relational with Char *
+  bool operator ==(const char * cs) {
+    int i{};
+    for(;cs[i];++i);
+    return equal(s.begin(),s.end(),cs,cs+i);
+  }
+  bool operator <(const char * cs){
+    int i{};
+    for(;cs[i];++i);
+    return lexicographical_compare(s.begin(),s.end(),cs,cs+i);
+  }
+
+  bool operator !=(const char * cs) {return !(*this==ob);}
+  bool operator >=(const char * cs) {return !(*this<ob);}
+  bool operator >(const char * cs) {return !(*this<ob && *this!=ob);}
+  bool operator <=(const char * cs) {return !(*this>ob);}
+
+  int strsize() const {return s.size();}
+  void makestr(char *str){
+    vector<char>::iterator p = s.begin();
+    while(p!=s.end()){
+     *s++ = *p++;
+    }
+  }
+
+  operator char *(){
+    int len = s.size();
+    char *tmp=new(nothrow) char[len+1];
+    if(tmp) exit(3);
+    for(int i=0;i<len;++i){
+      tmp[i]=s[i];
+    }
+    tmp[s.size]='\0';
+    return tmp;
+  }
 };
 
 StrType::StrType(){
@@ -67,4 +117,31 @@ StrType::StrType operator=(const char * cs){
   int i{};
   for(;cs[i];++i);
   s.assign(cs,cs+i);
+}
+
+StrType::StrType operator+(const StrType &ob){
+  StrType tmp;
+  tmps.s.assign(s.begin(),s.end());
+  vector<char>::iterator p=ob.s.begin();
+  while(p!=ob.s.end()){
+    tmp.s.push_back(*p++);
+  }
+  return tmp;
+}
+StrType::StrType operator+(const char* cs){
+  StrType tmp;
+  tmp.s.assign(s.begin(),s.end());
+  for(int i=0;cs[i];++i){
+    tmp.s.push_back(cs[i]);
+  }
+  return tmp;
+}
+
+StrType operator-(const StrType &ob){
+  
+}
+StrType operator-(const char* cs){
+  
+}
+
 }
